b672bac0e4091e513abcc30e2374ce027bf8e001
Florian REY <florianrey.info@gmail.com>
2023-11-25 00:36:46 +0100

    remove ccw terms, remove RussianDolls replace by BeveledLength

--- js/core.js
+++ js/core.js
@@ -20,7 +20,7 @@ const TAU_Q = Math.PI / 2;  // 90° in rad
 const FLOAT_PRECISION = 7;
 const FLOAT_2_STR_PRECISION = 2;
 
-const ASSEMBLY_METHODS = ["GoodKarma", "RussianDolls"]
+const ASSEMBLY_METHODS = ["GoodKarma", "BeveledLength"]
 const ASSEMBLY_DIRECTIONS = ["Clockwise Rotation", "Counterclockwise Rotation", "Symmetry Axis"]
 
 // -----------------------------------
