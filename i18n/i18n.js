e1761fc6f2a268d12899215164617caf630f2e9e
Florian REY <florianrey.info@gmail.com>
2023-12-05 20:41:07 +0100

    start to translate help in french

--- i18n/i18n.js
+++ i18n/i18n.js
@@ -3,8 +3,35 @@ const I18N_LANGUAGES = {
         name: "English",
         dictionary: {
             hello_world: "hello world",
-            display_help: "Display help",
-            help__number_order: "Number of kites starting on the crown at the top of the zome",
+            display_help: "display help",
+
+            // Common lexical
+            zome: "zome",
+            rhombuses: "rhombuses",
+            rhombus: "rhombus",
+            shape: "shape",
+            framework: "framework",
+
+            // Help
+            help_shape: "The shape of a zome is determined by :",
+            help_n_param: "Number of rhombuses rotating around the vertical axis",
+            help_m_param: "Number of rhombuses from top to bottom",
+            help_f_param: "Shape angle(Θ) | Number(F) which allows to calculate the first crown of zome. F = tan(Θ)",
+            help_hr_param: "The height ratio is the visible part of zome (Set HR to 1 for an entire zome)",
+            help_kr_param: "The kite ratio is the proportion between the upper and lower edges of a rhombus",
+
+
+            help_zh_param: "Height of the visible part of the zome",
+            help_tw_param: "Width of timbers",
+            help_tt_param: "Thickness of timbers",
+            help_st_param: "Strengthening of timbers",
+
+            help_am_param: "From left to right :",
+            help_am_param_1: "Xpansion direction",
+            help_am_param_2: "Assembly method",
+            help_am_param_3: "Assembly direction",
+
+            help_vp_param: "The Vanishing point is used to build geometries called 'GoodKarma'",
         },
 
     },
@@ -12,10 +39,35 @@ const I18N_LANGUAGES = {
         name: "Français",
         dictionary: {
             hello_world: "bonjour",
-            display_help: "Afficher l'aide",
-            help__number_order: "Le nombre de losanges sur la couronne en haut du zome",
-        },
+            display_help: "afficher l'aide",
 
+            // Common lexical
+            zome: "zome",
+            rhombuses: "losanges",
+            rhombus: "losange",
+            shape: "forme",
+            framework: "charpente",
+
+            // Help
+            help_shape: "La forme d'un zome est déterminée par :",
+            help_n_param: "Le nombre de losanges tournant autour de l'axe vertical",
+            help_m_param: "Le nombre de losanges de haut en bas",
+            help_f_param: "Angle de forme(Θ) | Nombre(F) qui permet de calculer la première couronne du zome. F = tan(Θ)",
+            help_hr_param: "Le ratio de la hauteur est La part visible du zome (Réglez HR sur 1 pour un zome entier)",
+            help_kr_param: "Le ratio du cerf-volant est la proportion entre les bords supérieurs et inférieurs d'un losange",
+
+            help_zh_param: "Hauteur de la part visible du zome",
+            help_tw_param: "Largeur des montants",
+            help_tt_param: "Profondeur des montants",
+            help_st_param: "Renforcement des montants",
+
+            help_am_param: "De gauche à droite :",
+            help_am_param_1: "Direction de l'expansion",
+            help_am_param_2: "Méthode d'assemblage",
+            help_am_param_3: "Direction de l'assemblage",
+
+            help_vp_param: "Le point de fuite est utilisé pour construire les géométries nommées GoodKarma",
+        },
     },
 
 
