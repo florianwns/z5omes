ac24492ac0cb01111ae095bf280bd4bbef62d0bd
Florian REY <florianrey.info@gmail.com>
2024-05-02 20:43:10 +0200

    code refactoring : Create two func : svg_text, and svg_polygon

--- js/core.js
+++ js/core.js
@@ -608,7 +608,25 @@ function circle_path(cx, cy, r) {
     return 'M ' + cx + ' ' + cy + ' m -' + r + ', 0 a ' + r + ',' + r + ' 0 1,1 ' + (r * 2) + ',0 a ' + r + ',' + r + ' 0 1,1 -' + (r * 2) + ',0';
 }
 
+function svg_polygon(points, stroke_width = 2, fill_color="#000000", fill_opacity='1'){
+    const polygon = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+    polygon.setAttribute('points', `${points}`);
+    polygon.setAttribute('stroke', 'black');
+    polygon.setAttribute('stroke-width', `${stroke_width}`);
+    polygon.setAttribute('fill-opacity', `${fill_opacity}`);
+    polygon.setAttribute('fill', fill_color);
+    return polygon
+}
 
+function svg_text(txt = "", x = 0, y = 0, font_size = 12, fill_color="#000000"){
+    const text = document.createElementNS("http://www.w3.org/2000/svg", "text");
+    text.setAttribute('x', `${x}`);
+    text.setAttribute('y', `${y}`);
+    text.setAttribute('font-size', `${font_size}`);
+    text.setAttribute('fill', `${fill_color}`);
+    text.innerHTML = `${txt}`;
+    return text;
+}
 // ---------------------------------
 // ========== Conversions ==========
 // ---------------------------------
